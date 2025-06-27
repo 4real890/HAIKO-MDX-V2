b382dfa4d3c1384dba0ec4f5da7cfe547cd76f2c
4real890 <handsdesouth@gmail.com>
2025-06-27 03:13:44 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴀɪᴋᴏ ᴍᴅx*
+  > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟɪɢᴀɴɢ ᴛᴇᴄʜs*
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
